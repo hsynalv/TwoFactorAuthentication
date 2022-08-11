a41ca9afe50d27921d96de0b7abe87e8dac2f517
hhsynalv <hhsynalv@gmail.com>
2022-08-11 14:20:56 +0300

    Member Main Page

--- NetCoreIdentity/Controllers/MemberController.cs
+++ NetCoreIdentity/Controllers/MemberController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Mapster;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NetCoreIdentity.Models;
+using NetCoreIdentity.Models.ViewModel;
 
 namespace NetCoreIdentity.Controllers
 {
     public class MemberController : Controller
     {
-        public IActionResult Index()
+
+        public UserManager<AppUser> userManager { get; }
+        public SignInManager<AppUser> signInManager { get; }
+
+        public MemberController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
         {
-            return View();
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+        
+        public async Task<IActionResult> Index()
+        {
+            AppUser user = await userManager.FindByNameAsync(User.Identity.Name);
+            UserViewModel userViewModel = user.Adapt<UserViewModel>();
+
+            return View(userViewModel);
         }
     }
 }
