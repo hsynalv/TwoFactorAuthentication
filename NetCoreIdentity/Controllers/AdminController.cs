afd4daa14081adf21b5a68c12bd2c3ec6ac5c897
hhsynalv <hhsynalv@gmail.com>
2022-08-15 15:52:39 +0300

    Roles CRUD

--- NetCoreIdentity/Controllers/AdminController.cs
+++ NetCoreIdentity/Controllers/AdminController.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Mapster;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.CodeAnalysis.VisualBasic.Syntax;
 using NetCoreIdentity.Models;
+using NetCoreIdentity.Models.ViewModel;
 
 namespace NetCoreIdentity.Controllers
 {
     public class AdminController : BaseController
     {
 
-        public AdminController(UserManager<AppUser> userManager) : base(userManager,null,null)
+        public AdminController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager) : base(userManager, roleManager)
         {
         }
 
@@ -22,5 +24,86 @@ namespace NetCoreIdentity.Controllers
         {
             return View(_userManager.Users.ToList());
         }
+
+        public IActionResult RoleCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RoleCreate(RoleViewModel roleViewModel)
+        {
+            IdentityRole role = new();
+            role.Name = roleViewModel.Name;
+            IdentityResult result = _roleManager.CreateAsync(role).Result;
+
+            if (result.Succeeded)
+
+            {
+                return RedirectToAction("Roles");
+            }
+            else
+            {
+                AddModelError(result);
+            }
+
+            return View(roleViewModel);
+        }
+
+        public IActionResult Roles()
+        {
+            return View(_roleManager.Roles.ToList());
+        }
+
+        public IActionResult RoleDelete(string id)
+        {
+            IdentityRole role = _roleManager.FindByIdAsync(id).Result;
+            if (role != null)
+            {
+                IdentityResult result = _roleManager.DeleteAsync(role).Result;
+            }
+
+            return RedirectToAction("Roles");
+        }
+
+        public IActionResult RoleUpdate(string id)
+        {
+            IdentityRole role = _roleManager.FindByIdAsync(id).Result;
+
+            if (role != null)
+            {
+                return View(role.Adapt<RoleViewModel>());
+            }
+
+            return RedirectToAction("Roles");
+        }
+
+        [HttpPost]
+        public IActionResult RoleUpdate(RoleViewModel roleViewModel)
+        {
+            IdentityRole role = _roleManager.FindByIdAsync(roleViewModel.Id).Result;
+
+            if (role != null)
+            {
+                role.Name = roleViewModel.Name;
+                IdentityResult result = _roleManager.UpdateAsync(role).Result;
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Roles");
+                }
+                else
+                {
+                    AddModelError(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Güncelleme işlemi başarısız oldu.");
+            }
+
+            return View(roleViewModel);
+        }
+
     }
 }
