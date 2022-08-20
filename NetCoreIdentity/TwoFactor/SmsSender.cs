fc835a0aa5c00fd9e781d02beabba461bae1e04a
hhsynalv <hhsynalv@gmail.com>
2022-08-21 00:11:51 +0300

    SmsSender.cs has been done

---
+++ NetCoreIdentity/TwoFactor/SmsSender.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Options;
+
+namespace NetCoreIdentity.TwoFactorServices;
+
+public class SmsSender
+{
+    private readonly TwoFactorOptions _twoFactorOptions;
+
+    private readonly TwoFactorService _twoFactorService;
+
+    public SmsSender(IOptions<TwoFactorOptions> options, TwoFactorService twoFactorService)
+    {
+        _twoFactorOptions = options.Value;
+        _twoFactorService = twoFactorService;
+    }
+
+    public string Send(string phone)
+    {
+        string code = _twoFactorService.GetCodeVerification().ToString();
+
+        //SMS PROVIDER CODES
+
+        return code;
+    }
+}
